500a1367bf19f66017e81f107580e8b989692a33
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-26 12:08:21 +0200

    adds drop path function

---
+++ maxvit/functions/drop_path.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def drop_path(
+    x: torch.Tensor, drop_prob: float = 0.0, training: bool = False
+) -> torch.Tensor:
+    if drop_prob == 0.0 or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+    mask = x.new_empty(shape).bernoulli_(keep_prob)
+    return x.div(keep_prob) * mask
